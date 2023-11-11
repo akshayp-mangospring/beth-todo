6efb345508336064af4404447e796d25993ff4cc
akshayp-mangospring <akshayp@mangospring.com>
2023-11-11 11:25:38 +0530

    Add Update and Delete of Todos

--- index.tsx
+++ index.tsx
@@ -1,4 +1,4 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
 import { html } from "@elysiajs/html";
 import * as elements from "typed-html";
 
@@ -16,16 +16,75 @@ const BaseHtml = ({ children }: elements.Children) => (
     ${children}
   </html>`);
 
+type Todo = {
+  id: number;
+  content: string;
+  completed: boolean;
+};
+
+const db: Todo[] = [
+  { id: 1, content: 'Learn React', completed: true },
+  { id: 2, content: 'Learn BETH stack', completed: false }
+];
+
 const app = new Elysia()
   .use(html())
   .get('/', ({ html }) => html(
     <BaseHtml>
-      <body class="flex w-full h-screen justify-center items-center">
-        <button hx-post="/clicked" hx-swap="outerHTML">Click Me</button>
-      </body>
+      <body
+        class="flex w-full h-screen justify-center items-center"
+        hx-get="/todos"
+        hx-trigger="load"
+        hx-swap="innerHTML"
+      />
     </BaseHtml>)
   )
   .post('/clicked', () => <div class="text-blue-600">Blood in my eyes dog and I can't see</div>)
+  .get('/todos', () => <TodoList todos={db} />)
+  .post('/todos/toggle/:id', ({ params }) => {
+    const todo = db.find((todo) => params.id === todo.id);
+
+    if (todo) {
+      todo.completed = !todo.completed;
+      return <TodoItem {...todo} />
+    }
+  },
+  {
+    params: t.Object({
+      id: t.Numeric(),
+    })
+  })
+  .delete('/todos/:id', ({params}) => {
+    const todo = db.find((todo) => params.id === todo.id);
+
+    if (todo) {
+      db.splice(db.indexOf(todo), 1)
+    }
+  },{
+    params: t.Object({
+      id: t.Numeric(),
+    })
+  })
   .listen(3000);
+
+function TodoItem({ id, content, completed }: Todo) {
+  return (
+    <li data-id={id} class="flex flex-row space-x-3">
+      <input hx-post={`/todos/toggle/${id}`} hx-target="closest li" hx-swap="outerHTML" type="checkbox" checked={completed} />
+      <span>{content}</span>
+      <button hx-delete={`/todos/${id}`} hx-swap="outerHTML" hx-target="closest li" class="text-red-500">X</button>
+    </li>
+  );
+}
+
+function TodoList({ todos }: { todos: Todo[] }) {
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <TodoItem {...todo} />
+      ))}
+    </ul>
+  );
+}
 
 console.log(`Elysia is running at http://${app.server?.hostname}:${app.server?.port}`);
